78b3567c3cfd4e2b7b9c4164c0653f5a76444f22
Tombeau Chrisnor <tombeauc@gmail.com>
2023-10-15 05:02:06 -0400

    Travail sur le lien de navigation

--- eleva.js
+++ eleva.js
@@ -1,11 +1,26 @@
 var font1 = document.getElementById("font1");
 var font2 = document.getElementById("font2");
+var me = document.getElementById("me");
+var fc = document.getElementById("fc");
+
+me.style.display=none;
+fc.style.display=none;
 
 font2.style.display ="none";
 
+showImage1()
+{
+    font1.style.display="block";
+    font2.style.display="none";
+}
+
+showImage2()
+{
+    font2.style.display="block";
+    font1.style.display="none";
+}
+
 setInterval(function(){
-    if(font2.style.display=="none")
-        font1.style.display="block";
-    else if(font2.style.display=="block")
-        font1.style.display="none";
+    font2.style.display="block";
+    font1.style.display="none";
 }, 5000)
